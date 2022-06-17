76be414c89a1631d4fa0e04323996dcd575b4762
ilysim <illymarie1@live.com>
2022-06-16 22:19:49 -0500

    finish touch ups

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express Skills' })
+  res.render('index', { title: 'Dev Skills Lab' })
 })
 
 export { 
